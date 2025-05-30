37813d0636cdb71c031866ef14b50c0242073e1f
IgnacioBA9 <150173378+IgnacioBA9@users.noreply.github.com>
2025-05-30 10:23:41 +0000

    [fix:] Minor cover fix

--- src/js/app.js
+++ src/js/app.js
@@ -26,21 +26,37 @@ function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class=${variables.includeCover}><img src="${variables.background}" /></div>`;
+  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1> ${variables.name == null ? "Lucy" : variables.name} ${variables.lastName == null ? "Garcia" : variables.lastName}</h1>
+          <h1> ${variables.name == null ? "Lucy" : variables.name} ${
+    variables.lastName == null ? "Garcia" : variables.lastName
+  }</h1>
           <h2>${variables.role == null ? "Web developer" : variables.role}</h2>
-          <h3>${variables.city == null ? "Madrid" : variables.city}, ${variables.country == null ? "España" : variables.country}</h3>
-          <ul class=${variables.socialMediaPosition == "position-left" ? variables.socialMediaPosition : variables.socialMediaPosition = "position-right"}>
-            <li><a href=${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href=${variables.github}><i class="fab fa-github"></i></a></li>
-            <li><a href=${variables.linkedin}><i class="fab fa-linkedin"></i></a></li>
-            <li><a href=${variables.instagram}><i class="fab fa-instagram"></i></a></li>
+          <h3>${variables.city == null ? "Madrid" : variables.city}, ${
+    variables.country == null ? "España" : variables.country
+  }</h3>
+          <ul class=${
+            variables.socialMediaPosition == "position-left"
+              ? variables.socialMediaPosition
+              : (variables.socialMediaPosition = "position-right")
+          }>
+            <li><a href=${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${
+              variables.github
+            }><i class="fab fa-github"></i></a></li>
+            <li><a href=${
+              variables.linkedin
+            }><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=${
+              variables.instagram
+            }><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
